bdbdfecff2864f25c4d7675c37878eab30bbb145
indra prasetyo <indra.prasetyo@calgaryscientific.com>
2016-02-13 15:45:47 -0700

    WIP on materials zephyr site

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<!DOCTYPE HTML>
+<!doctype html>
 <?php
 include 'config.php';
 ini_set('error_reporting', E_ALL);
@@ -30,87 +30,112 @@ if ($connected->connect_errno > 0) {
 	/* close connection */
 	$connected->close();
 ?>
-<html>
-	<head>
-		<meta name="description" content="Trackking application used to check status of the Zephyr bike in real time." />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-		<meta name="keywords" content="ZEUS, Zephyr, NovAtel, tracker" />
-		<meta name="viewport" content="initial-scale=1.0">
-		<meta charset="utf-8">
-		<title>ZEUS Zephyr Tracker</title>
-		<link rel="stylesheet" type="text/css" href="styles.css" title="style" />
-        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
-        <script src="https://maps.googleapis.com/maps/api/js?v=3.11&sensor=false" type="text/javascript"></script>
-        <script type="text/javascript">
-        // check DOM Ready
-        $(document).ready(function() {
-            // execute
-			(function() {
-				// data points
-				// [name, latitude, longitude, altitude, time, velocity, acceleration]
-				var markerData = <?php echo json_encode($data_array) ?>; 
-				console.log(markerData);
-	
-console.log(markerData[0][1]);
-                // map options
-                var options = {
-				    tilt: 0,
-                    zoom: 18,
-                    center: new google.maps.LatLng(markerData[0]["latitude"], markerData[0]["longitude"]),
-                    mapTypeId: google.maps.MapTypeId.SATELLITE,
-                };
 
-                // init map
-                var map = new google.maps.Map(document.getElementById('map_canvas'), options);
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="Website to track ZEUS bike">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>ZEUS Zephyr Tracker</title>
 
-				// Modify marker point appearance
-				var pinIcon = new google.maps.MarkerImage(
+    <!-- Add to homescreen for Chrome on Android -->
+    <meta name="mobile-web-app-capable" content="yes">
+    <link rel="icon" sizes="192x192" href="images/android-desktop.png">
+
+    <!-- Add to homescreen for Safari on iOS -->
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black">
+    <meta name="apple-mobile-web-app-title" content="Material Design Lite">
+    <link rel="apple-touch-icon-precomposed" href="images/ios-desktop.png">
+
+    <!-- Tile icon for Win8 (144x144 + tile color) -->
+    <meta name="msapplication-TileImage" content="images/touch/ms-touch-icon-144x144-precomposed.png">
+    <meta name="msapplication-TileColor" content="#3372DF">
+
+    <link rel="shortcut icon" href="images/favicon.png">
+
+    <!-- SEO: If your mobile URL is different from the desktop URL, add a canonical link to the desktop page https://developers.google.com/webmasters/smartphone-sites/feature-phones -->
+    <!--
+    <link rel="canonical" href="http://www.example.com/">
+    -->
+
+    <link href="https://fonts.googleapis.com/css?family=Roboto:regular,bold,italic,thin,light,bolditalic,black,medium&amp;lang=en" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="material.min.css">
+    <link rel="stylesheet" href="styles.css">
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+	<script src="https://maps.googleapis.com/maps/api/js?v=3.11&sensor=false" type="text/javascript"></script>
+	<script type="text/javascript">
+	// check DOM Ready
+	$(document).ready(function() {
+		// execute
+		(function() {
+			// data points
+			// [name, latitude, longitude, altitude, time, velocity, acceleration]
+			var markerData = <?php echo json_encode($data_array) ?>; 
+			console.log(markerData);
+
+			console.log(markerData[0][1]);
+			// map options
+			var options = {
+				tilt: 0,
+				zoom: 18,
+				center: new google.maps.LatLng(markerData[0]["latitude"], markerData[0]["longitude"]),
+				mapTypeId: google.maps.MapTypeId.SATELLITE,
+			};
+
+			// init map
+			var map = new google.maps.Map(document.getElementById('map_canvas'), options);
+
+			// Modify marker point appearance
+			var pinIcon = new google.maps.MarkerImage(
 					"http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|FFFF00",
 					null, 
 					null,
 					null,
-				new google.maps.Size(18, 30)
-				);  
+					new google.maps.Size(18, 30)
+					);  
 
-				var pinIconStart = new google.maps.MarkerImage(
+			var pinIconStart = new google.maps.MarkerImage(
 					"http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|00FF00",
 					null, 
 					null,
 					null,
-				new google.maps.Size(18, 30)
-				); 
+					new google.maps.Size(18, 30)
+					); 
 
-				var pinIconLast = new google.maps.MarkerImage(
+			var pinIconLast = new google.maps.MarkerImage(
 					"http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|FF0000",
 					null, 
 					null,
 					null,
-				new google.maps.Size(18, 30)
-				);
-				// Initialize array to contain travel path
-				var travelPath = new Array();
-				var origin = new google.maps.LatLng(markerData[0]["latitude"], markerData[0]["longitude"]);
+					new google.maps.Size(18, 30)
+					);
+			// Initialize array to contain travel path
+			var travelPath = new Array();
+			var origin = new google.maps.LatLng(markerData[0]["latitude"], markerData[0]["longitude"]);
 
-                for (var i = 0; i < markerData.length; i++) {
-                    // Initialize markers
-                    var marker = new google.maps.Marker({
-                        position: new google.maps.LatLng(markerData[i]["latitude"], markerData[i]["longitude"]),
-                        map: map,
-                        title: 'Data Point: ' + i
-                    });
-                	var markerPath = new google.maps.LatLng(markerData[i]["latitude"], markerData[i]["longitude"]);
-					if(i == 0) {
-						marker.setIcon(pinIconStart);
-					} else {
-						marker.setIcon(pinIcon);
-					}
-					travelPath.push(markerPath);
+			for (var i = 0; i < markerData.length; i++) {
+				// Initialize markers
+				var marker = new google.maps.Marker({
+					position: new google.maps.LatLng(markerData[i]["latitude"], markerData[i]["longitude"]),
+					map: map,
+					title: 'Data Point: ' + i
+				});
+				var markerPath = new google.maps.LatLng(markerData[i]["latitude"], markerData[i]["longitude"]);
+				if(i == 0) {
+					marker.setIcon(pinIconStart);
+				} else {
+					marker.setIcon(pinIcon);
+				}
+				travelPath.push(markerPath);
 
-                    // Process multiple info windows
-                    (function(marker, i) {
-                        // Add click event
-                        google.maps.event.addListener(marker, 'click', function() {
-                            infowindow = new google.maps.InfoWindow({
+				// Process multiple info windows
+				(function(marker, i) {
+					// Add click event
+					google.maps.event.addListener(marker, 'click', function() {
+						infowindow = new google.maps.InfoWindow({
 							content: '<b>Data Point:</b> ' + i +
 								'<br>Latitude: ' + markerData[i]["latitude"] +
 								'<br>Longitude: ' + markerData[i]["longitude"] +
@@ -118,81 +143,108 @@ console.log(markerData[0][1]);
 								'<br>Accelertaion: ' + markerData[i]["acceleration"] +
 								'<br>Time: ' + markerData[i]["time"] +
 								'<br>Velocity: ' + markerData[i]["velocity"] 
-                            });
-                            infowindow.open(map, marker);
-                        });
-                    })(marker, i);
-                }
+						});
+						infowindow.open(map, marker);
+					});
+				})(marker, i);
+			}
 
-				// Initialize travel path properties
-				var pathProperties=new google.maps.Polyline({
-					path:travelPath,
-					strokeColor:"#0000FF",
-					strokeOpacity:0.8,
-					strokeWeight:2
-				});
-				console.log(pathProperties);
-				pathProperties.setMap(map);
+			// Initialize travel path properties
+			var pathProperties=new google.maps.Polyline({
+				path:travelPath,
+				strokeColor:"#0000FF",
+				strokeOpacity:0.8,
+				strokeWeight:2
+			});
+			console.log(pathProperties);
+			pathProperties.setMap(map);
 
-				// Event for resetting to origin on button press
-				google.maps.event.addDomListener(document.getElementById("reset"), 'click', function() {
-					map.setCenter(origin);
-					map.setZoom(30);
-				});
+			// Event for resetting to origin on button press
+			google.maps.event.addDomListener(document.getElementById("reset"), 'click', function() {
+				map.setCenter(origin);
+				map.setZoom(30);
+			});
 
-            })();
-        });
-        </script>
-	</head>
-	<body>
-		<div id="main">
-			<div id="header">
-				<div id="logo">
-					<div id="logo_text">
-						<!-- class="logo_colour", allows you to change the colour of the text -->
-						<h1><a href="index.php">ZEUS</a></h1>
-						<under>Zephyr Tracker</under>
-					</div>
-				</div>
-				<div id="menubar">
-					<ul id="menu">
-						<!-- put class="selected" in the li tag for the selected page - to highlight which page you're on -->
-						<li class="selected"><a href="index.php">Home</a></li>
-						<li><a href="about.php">About</a></li>
-						<li><a href="allraces.php">All Races</a></li>
-<?php
-if (empty($_SESSION["username"])) {
-?>
-			 <li><a href="loginpage.php">Login</a></li>
-<?php
-} else if (!empty($_SESSION["username"])) {
-?>
-	<li><a href = "setuprace.php">Setup/Modify Race</a></li>
-	<li><a href="usercp.php">User CP: <?php
-	if (!empty($_SESSION["username"])) {
-		echo $_SESSION["username"];
-	}
-?></a></li> 
-		   <li><a href="logout.php">Logout</a></li>              
-<?php
+		})();
+	});
+	</script>
+
+	<style>
+body, html {
+  height: 100%;
+  width: 100%;
 }
-?>
-					</ul>
-				</div>
-			</div>
-			<div id="site_content">
-				<div id="content">
-				<Bodybold>Most Recent Data Set</bodybold>
-					<br></br>
-						<div id="map_canvas" style="width: 800px; height:500px;"></div>
-						<input type="button" id="reset" value="Reset to origin"></input>
 
-				</div>
-			</div>
-		<div id="content_footer"></div>
-	<div id="footer">
-	<a>Created by the University of Calgary ZEUS Racing Team</a>
-	</div>
-	</div>
-	</body>
+div#content {
+  width: 100%; height: 100%;
+}
+	#view-source {
+	  position: fixed;
+	  display: block;
+	  right: 0;
+	  bottom: 0;
+	  margin-right: 40px;
+	  margin-bottom: 40px;
+	  z-index: 900;
+	}
+	</style>
+  </head>
+  <body>
+    <div class="demo-layout mdl-layout mdl-js-layout mdl-layout--fixed-drawer mdl-layout--fixed-header">
+      <header class="demo-header mdl-layout__header mdl-color--grey-100 mdl-color-text--grey-600">
+        <div class="mdl-layout__header-row">
+          <span class="mdl-layout-title">Home</span>
+          <div class="mdl-layout-spacer"></div>
+          <div class="mdl-textfield mdl-js-textfield mdl-textfield--expandable">
+            <label class="mdl-button mdl-js-button mdl-button--icon" for="search">
+              <i class="material-icons">search</i>
+            </label>
+            <div class="mdl-textfield__expandable-holder">
+              <input class="mdl-textfield__input" type="text" id="search">
+              <label class="mdl-textfield__label" for="search">Enter your query...</label>
+            </div>
+          </div>
+          <button class="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--icon" id="hdrbtn">
+            <i class="material-icons">more_vert</i>
+          </button>
+          <ul class="mdl-menu mdl-js-menu mdl-js-ripple-effect mdl-menu--bottom-right" for="hdrbtn">
+            <li class="mdl-menu__item">About</li>
+            <li class="mdl-menu__item">Contact</li>
+            <li class="mdl-menu__item">Legal information</li>
+          </ul>
+        </div>
+      </header>
+      <div class="demo-drawer mdl-layout__drawer mdl-color--blue-grey-900 mdl-color-text--blue-grey-50">
+        <header class="demo-drawer-header">
+          <img src="images/user.jpg" class="demo-avatar">
+          <div class="demo-avatar-dropdown">
+            <span>dani@aqt.com</span>
+            <div class="mdl-layout-spacer"></div>
+            <button id="accbtn" class="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--icon">
+              <i class="material-icons" role="presentation">arrow_drop_down</i>
+              <span class="visuallyhidden">Accounts</span>
+            </button>
+            <ul class="mdl-menu mdl-menu--bottom-right mdl-js-menu mdl-js-ripple-effect" for="accbtn">
+              <li class="mdl-menu__item">User Control Panel</li>
+            </ul>
+          </div>
+        </header>
+        <nav class="demo-navigation mdl-navigation mdl-color--blue-grey-800">
+          <a class="mdl-navigation__link" href=""><i class="mdl-color-text--blue-grey-400 material-icons" role="presentation">home</i>Home</a>
+          <a class="mdl-navigation__link" href="about.php"><i class="mdl-color-text--blue-grey-400 material-icons" role="presentation">motorcycle</i>About</a>
+		  <a class="mdl-navigation__link" href=""><i class="mdl-color-text--blue-grey-400 material-icons" role="presentation">list</i>All Races</a>
+          <a class="mdl-navigation__link" href=""><i class="mdl-color-text--blue-grey-400 material-icons" role="presentation">build</i>Setup/Modify Race</a>
+          <a class="mdl-navigation__link" href=""><i class="mdl-color-text--blue-grey-400 material-icons" role="presentation">person</i>Login</a>
+          <a class="mdl-navigation__link" href=""><i class="mdl-color-text--blue-grey-400 material-icons" role="presentation">person_outline</i>Logout</a>
+          <div class="mdl-layout-spacer"></div>
+          <a class="mdl-navigation__link" href=""><i class="mdl-color-text--blue-grey-400 material-icons" role="presentation">help_outline</i><span class="visuallyhidden">Help</span></a>
+        </nav>
+	  </div>
+	  <main class="mdl-layout__content mdl-color--white-100">
+		<div id="map_canvas" style="width: 100%; height:500px;"></div>
+		<input type="button" id="reset" value="Reset to origin"></input>
+	  </main>
+    </div>
+    <script src="material.min.js"></script>
+  </body>
 </html>
